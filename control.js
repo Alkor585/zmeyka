58b77794ed3daa3ba756019182d5b61af28e9c0d
Dunice Practice <dunicepractice@Dunices-Mac-mini.local>
2021-09-21 13:04:20 +0300

    Почти готово

--- control.js
+++ control.js
@@ -1,10 +1,4 @@
-function fruitAdd() {
-    let fruitX = getRandomIntInclusive(0, colCount - 1);
-    let fruitY = getRandomIntInclusive(0, rowCount - 1);
-  
-    if (!snakeCoords.some(([x, y]) => fruitX === x && fruitY === y)) {
-      cells.find((cell) => cell.row === fruitY && cell.col === fruitX).el.classList.add('fruit')
-    }
-    setTimeout(fruitAdd, 6000);
-  
-  }
+if (!cells.find(cell) => cell.isFruit) {
+    addFruit()
+
+}
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const button = document.querySelector('#pressMe');
 const secretblock = document.querySelector('#secretBlock');
+const sign = document.querySelector('#signn');
+const buttonR = document.querySelector('#buttonRestart')
 function pressed() {
+
   button.classList.add('hidden');
   secretblock.classList.remove('hidden');
   createGameField();
   tick();
 }
 button.addEventListener('click', pressed);
+buttonR.addEventListener('click', buttonRestartPressed);
 
 const cells = [];
 let snakeCoords = [[25, 20], [25, 21], [25, 22], [25, 23], [25, 24], [25, 25], [25, 26], [25, 27], [25, 28], [25, 29], [25, 30]];
@@ -14,6 +18,14 @@ const colCount = 50;
 const rowCount = 50;
 
 function createGameField() {
+  function monitorSettings() {
+    const cellSize=Math.floor(Math.min(window.innerHeight, window.innerWidth) / 50)
+  
+
+    gameField.style['grid-template-columns'] = `repeat(50, ${cellSize}px)`;
+    gameField.style['grid-template-rows'] = `repeat(50, ${cellSize}px)`;
+  }
+  monitorSettings();
   const container = document.querySelector('#gameField');
   for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
     for (let colIndex = 0; colIndex < colCount; colIndex++) {
@@ -36,17 +48,23 @@ function createGameField() {
 }
 
 let n = 0;
+let needGrowUp = false;
+let fruitExistNumber = 0;
+const maxFruitExistNumber = 3;
+
 
 function tick() {
   move();
-  if (n % 12 === 0) fruitAdd();
-  const head = snakeCoords[snakeCoords.length - 1]
-  if (cells.find((cell) => cell.col === head[0] && cell.row === head[1] && cell.isFruit)) {
+  const head = snakeCoords[snakeCoords.length - 1];
 
-    cells.find((cell) => cell.el.classList.remove('head',move),
-    cell.el.classList.add('head',move))
+  if (fruitExistNumber < maxFruitExistNumber) fruitAdd();
+  const fruitCell = cells.find((cell) => cell.col === head[0] && cell.row === head[1] && cell.isFruit);
 
-  }
+  if (fruitCell != null) {
+    fruitCell.isFruit = false;
+    needGrowUp = true;
+    fruitExistNumber -= 1;
+  } else needGrowUp = false;
   for (const cell of cells) {
     if (snakeCoords.some(([x, y]) => x === cell.col && y === cell.row)) {
       cell.el.classList.add('snake');
@@ -54,17 +72,29 @@ function tick() {
       cell.el.classList.remove('snake');
     }
     if (cell.isFruit) cell.el.classList.add('fruit');
+    else cell.el.classList.remove('fruit');
   }
+
   n += 1;
-  setTimeout(tick, 200);
+  const snakeIsDeath = !checkSnakeIsAlive(head);
+  if (snakeIsDeath) {
+    secretblock.classList.add('hidden');
+    sign.classList.remove('hidden');
+    sign.classList.add('sign');
+    buttonR.classList.remove('hidden')
+    buttonR.classList.add('buttonRestart')
+  }
+  if (!snakeIsDeath) setTimeout(tick, 100);
 }
+
 
 const prevMoveDelta = [0, 1];
 const moveDelta = [0, 1];
 
 
 function move() {
-  snakeCoords.shift();
+  if (!needGrowUp) snakeCoords.shift();
+
   const head = snakeCoords[snakeCoords.length - 1];
   snakeCoords.push([head[0] + moveDelta[0], head[1] + moveDelta[1]]);
   prevMoveDelta[0] = moveDelta[0];
@@ -109,15 +139,28 @@ function getRandomIntInclusive(min, max) {
 function fruitAdd() {
   let fruitX = getRandomIntInclusive(0, colCount - 1);
   let fruitY = getRandomIntInclusive(0, rowCount - 1);
-
   if (!snakeCoords.some(([x, y]) => fruitX === x && fruitY === y)) {
+    fruitExistNumber += 1;
     const cell = cells.find((cell) => cell.row === fruitY && cell.col === fruitX);
     cell.isFruit = true;
-    console.log(fruitX, fruitY);
+
   }
+
 }
 
-function fruitRemove() {
-  cell.isFruit.classList.remove('fruit');
-  cell.isFruit.classList.add('cell');
+const notGameField = !cells.some((cell) => cell.row === y && cell.col === x)
+
+function checkSnakeIsAlive([headX, headY]) {
+  console.log(headX, headY)
+  if (headX < 0 || headX >= 50 || headY < 0 || headY >= 50) {
+    return false;
+  }
+
+  return !snakeCoords.some(([x, y], index) => (
+    index !== snakeCoords.length - 1 && headX === x && headY === y
+  ));
 }
+function buttonRestartPressed(){
+ window.location.reload()
+}
+ 
